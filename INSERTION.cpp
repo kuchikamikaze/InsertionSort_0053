812ba5db3c12df6b6df4058cb79bbc98161d0efd
Naufal Arif <na713634@gmail.com>
2026-03-12 18:58:11 +0800

    prosedur input

--- INSERTION.cpp
+++ INSERTION.cpp
@@ -3,3 +3,27 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input() { // procedure input
+    while (true) {
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20) {
+            break;
+        }
+        else {
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+
+    cout << endl;
+    cout << "===============================" << endl;
+    cout << "=========Masukan Element Array=========" << endl;
+    cout << "===============================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
